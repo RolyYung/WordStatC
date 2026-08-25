b17ae8eb6f410874744459e93a9d4079c624c3b9
roly <493782517@qq.com>
2026-08-25 16:55:21 +0800

    add: 判断读取的是英文字母还是其他字符.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-#define PATH_MAX_LENGTH 100
+#define PATH_MAX_LENGTH 10
+
+int is_alpha(int c){
+    if(c >= 'a' && c <= 'z'){
+        return 1;
+    }
+    if(c >= 'A' && c <= 'Z'){
+        return 1;
+    }
+    return 0;
+}
 
 int main(){
     // 提示用户输入文本文件路径
     printf("请输入文本路径:");
     char filePath[PATH_MAX_LENGTH];
     fgets(filePath, PATH_MAX_LENGTH, stdin);
+    int path_len = strlen(filePath);
+    if(path_len == PATH_MAX_LENGTH - 1 && filePath[path_len - 1] != '\n'){
+        printf("输入路径过长");
+        return -1;
+    }
     filePath[strcspn(filePath, "\n")] = '\0';
-    // scanf("%s", filePath);
     FILE * pFile = fopen(filePath, "r");
     if (pFile!=NULL)
     {
@@ -18,7 +32,11 @@ int main(){
         int result = fgetc(pFile);
         while (result != EOF)
         {
-            printf("%c", result);
+            if(is_alpha(result)){
+                printf("字符: %c \n", result);
+            }else{
+                printf("分隔符: %c \n", result);
+            }
             result = fgetc(pFile);
         }
         if(feof(pFile)){
@@ -27,6 +45,7 @@ int main(){
         }
         if(ferror(pFile)){
             printf("读取文件失败 \n");
+            fclose(pFile);
             return -1;
         }
         fclose (pFile);
