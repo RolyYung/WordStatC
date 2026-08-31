c9c831df7ec785a479d3a9283c1673cd20c356a8
roly <493782517@qq.com>
2026-08-31 14:38:19 +0800

    add: 用户输入前N个单词统计功能

--- src/main.c
+++ src/main.c
@@ -14,12 +14,16 @@ Word word_stat_arr[MAX_UNIQUE_WORD];
 static int word_stat_index = 0;
 static int word_total = 0;
 
+static int top_word_number = 0; // 统计的前N个单词. 由用户输入
+
 int to_lower(char ch);
 int is_alpha(int c);
 
 int stat_word_func(const char * str);
 void print_word_stats(void);
 void sort_words(void);
+int is_correct_top_word_number(void);
+int is_empty_word_file(void);
 
 
 int main(){
@@ -57,7 +61,7 @@ int main(){
             }else{
                 word[index] = '\0';
                 if(index > 0){
-                    printf("单词: %s \n", word);
+                    
                     int ret = stat_word_func(word);
                     if(ret == -1){
                         printf("不同单词数量超过上限");
@@ -66,13 +70,12 @@ int main(){
                     }
                     ++word_total;
                 }
-                printf("分隔符: %c \n", ch);
+                
                 index = 0;
             }
             ch = fgetc(pFile);
         }
         if(feof(pFile)){
-            printf("\n");
             if(index > 0){
                 word[index] = '\0';
                 printf("单词: %s \n", word);
@@ -97,7 +100,22 @@ int main(){
         printf("\n");
         printf("读取文件操作完成 \n");
         sort_words();
-        print_word_stats();
+
+        if(is_empty_word_file()){
+            return 0;
+        }
+        printf("\n");
+        printf("----------- \n");
+        printf("*********** \n");
+        printf("\n");
+        printf("总共有%d个单词 \n", word_stat_index);
+        printf("请输入你想看的前N名单词: ");
+        scanf("%d", &top_word_number);
+        if(is_correct_top_word_number()){
+            print_word_stats();
+        }else{
+            return 0;
+        }
         
     }else{
         printf("\n");
@@ -183,20 +201,39 @@ void sort_words(void){
     return ;
 }
 
+// 判断用户输入的数字是否合规
+int is_correct_top_word_number(void){
+    if(top_word_number <= 0){
+        printf("错误数字, 统计失败 \n");
+        return 0;
+    }
+    // 超过当前统计数,返回false
+    if(top_word_number > word_stat_index){
+        printf("超过不同单词数, 统计失败 \n");
+        return 0;
+    }
+    return 1;
+}
+
+int is_empty_word_file(){
+    if(word_total == 0){
+        printf("文件中没有可识别的单词");
+        return 1;
+    }
+    return 0;
+}
+
 // 打印单词统计
 void print_word_stats(void){
-    if(word_total == 0){
-        printf("文件为空, 没有单词");
-        return ;
-    }
-    printf("当前文件的单词个数: %d \n", word_total);
-    printf("不同单词数：%d \n", word_stat_index);
-    for (int i = 0; i < word_stat_index; i++)
+    printf("🧮 当前文件的单词个数: %d \n", word_total);
+    printf("📉 不同单词数：%d \n", word_stat_index);
+    printf("\n");
+    printf("排名前%d个单词的统计: \n", top_word_number);
+    for (int i = 0; i < top_word_number; i++)
     {
         Word word = word_stat_arr[i];
         float frequency = (float)word.count / word_total;
-        printf("单词: %s, 出现次数: %d \n", word.word, word.count);
-        printf("单词: %s, 频率: %.4f \n", word.word, frequency);
+        printf("🗒️ \n单词: %s, 出现次数: %d, 频率: %.4f \n", word.word, word.count, frequency);
     }
     
     
